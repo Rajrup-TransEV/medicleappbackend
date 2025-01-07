42f6303a620425977446d52d21adb6e585b0f084
Rajrup-TransEV <transmogrify17@outlook.com>
2025-01-07 11:08:45 +0530

    entered database

--- controller/appoinmentops/getallappoinmentdetails.py
+++ controller/appoinmentops/getallappoinmentdetails.py
@@ -16,4 +16,31 @@ getallappoinmentbp = Blueprint('getallappoinmentbp',__name__)
 
 @getallappoinmentbp.route('/getallappoinment',methods=['GET'])
 def getallappnfn():
-    pass
+    try:
+        db = get_db_connection()
+        allappoinmentcollections = db['appoinments']
+        doctor_collection = db['doctors']
+        patient_collections = db['patients']
+        appoinmentfinds = allappoinmentcollections.find()
+        appoinmentlist = []
+        
+     
+        for appoinment in appoinmentfinds:
+            doctor = doctor_collection.find_one({"uid": appoinment.get('doctorid')})
+            patient = patient_collections.find_one({"uid":appoinment.get('patientid')})
+            appoinment_data = {
+                'uid':appoinment.get('uid'),
+                'doctor_fullname': doctor['fullname'] if doctor else None,
+                'doctorid':appoinment.get('doctorid'),
+                'appoinmenttime':appoinment.get('appoinmenttime'),
+                'appoinmentdetails':appoinment.get('appoinmentdetails'),
+                'patientid':appoinment.get('patientid'),
+                'patient_firstname':patient['firstname'] if patient else None,
+                'patient_lastname':patient['lastname'] if patient else None,
+                'created_at':appoinment.get('created_at'),
+            }
+            appoinmentlist.append(appoinment_data)
+            return jsonify({"message":"Appoinment data hasbeen fetched successfully","data":appoinmentlist})
+    except Exception as e:
+        generatelogs("error",f"{str(e)}","getallappoinmentdetails.py")
+        return jsonify({"message":"Error fetching leave data Internal server error"}),500
