2c3ebf5b86559eba0b92e884b175c608c83cb808
Rajrup-TransEV <transmogrify17@outlook.com>
2025-01-07 11:20:42 +0530

    leave deleted successful

---
+++ controller/appoinmentops/updateappoinmentops.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, jsonify, request
+import os
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+from utils.logs import generatelogs
+
+
+def get_db_connection():
+        client = MongoClient(os.getenv('MONGODB_URI'))
+        db = client[os.getenv('DB_NAME')]
+        return db
+
+updateappoinmentops = Blueprint('updateappoinmentops',__name__)
+
+@updateappoinmentops.route('/update/appoinment',methods=['POST'])
+def updateappnfn():
+        pass
