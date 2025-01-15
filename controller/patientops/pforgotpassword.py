38cd4a9ee8f7611015710c5672f1ed9ccf6090b1
Rajrup-TransEV <transmogrify17@outlook.com>
2025-01-15 17:17:28 +0530

    optimization added

--- controller/patientops/pforgotpassword.py
+++ controller/patientops/pforgotpassword.py
@@ -7,7 +7,6 @@ from flask import Blueprint, request, jsonify
 from lib.emailsender import email_sender
 from utils.logs import generatelogs
 from pymongo import MongoClient
-from pymongo.errors import PyMongoError
 
 # Create a Blueprint for superadmin password reset
 patientpasswordreset_bp = Blueprint("patientpasswordreset_bp", __name__)
@@ -17,16 +16,9 @@ temp_storage = {}
 
 # Function to connect to MongoDB
 def get_db_connection():
-    try:
         client = MongoClient(os.getenv('MONGODB_URI'))
         db = client[os.getenv('DB_NAME')]
         return db
-    except PyMongoError as e:
-        messagetype = 'error'
-        message = f"Database connection error: {str(e)}"
-        filelocation = 'patientops/pforgotpassword.py'
-        generatelogs(messagetype, message, filelocation)
-        raise
 
 # Password reset logic for superadmin
 @patientpasswordreset_bp.route("/patientpasswordreset", methods=["POST"])
