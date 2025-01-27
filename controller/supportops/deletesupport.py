df06064e22ae8d0fd3ecef45fe5a1661af2b42cd
Rajrup-TransEV <transmogrify17@outlook.com>
2025-01-27 09:44:03 +0530

    delete support added

--- controller/supportops/deletesupport.py
+++ controller/supportops/deletesupport.py
@@ -16,4 +16,19 @@ deletesupportdetailsbp = Blueprint('deletesupportdetailsbp',__name__)
 
 @deletesupportdetailsbp.route('/ops/deletesupport',methods=['POST'])
 def deletesupportfn():
-    pass
+    supportid = str(request.form.get('supportid'))
+    try:
+        db = get_db_connection()
+        supportcol = db['support']
+        support = supportcol.find_one({"uid":supportid})
+        if support:
+            supportcol.delete_one({"uid":supportid})
+            generatelogs('info','Support data hasbeen deleted successfully','deletesupport.py')
+            return jsonify({"message":"support data deleted successfully"}),200
+        else:
+            generatelogs('error','support not found','deletesupport.py')
+            return jsonify({"message":"support data not found"}),404
+    except Exception as e:
+        print(e)
+        generatelogs("error",f"{str(e)}",'deletesupport.py')
+        return jsonify({"message","error deleting support"})
