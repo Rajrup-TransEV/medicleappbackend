48ba6e970e02c1408295e1a8b7a213b60088c869
L10Transev <transmogrify17@outlook.com>
2025-06-04 09:28:39 +0000

    uh
    Signed-off-by: L10Transev <transmogrify17@outlook.com>

--- controller/labops/labreportgetbyid.py
+++ controller/labops/labreportgetbyid.py
@@ -1,3 +1,21 @@
+from flask import Blueprint, jsonify, request
+import os
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+from utils.logs import generatelogs
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+def get_db_connection():
+    client = MongoClient(os.getenv('MONGODB_URI'))
+    db = client[os.getenv('DB_NAME')]
+    return db
+
+labreportbyidbp = Blueprint('labreportbyidbp',__name__)
+
 @labreportbyidbp.route('/labreport/labbyid', methods=['POST'])
 def labreportfn():
     labreportid = str(request.form.get('labreportid'))
