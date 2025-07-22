564adc9697c7d2fb4cc4923cfb68c776a0c21d0c
Rajrup-TransEV <transmogrify17@outlook.com>
2025-07-22 10:03:56 +0530

    s

--- server.py
+++ server.py
@@ -141,7 +141,6 @@ from controller.doctorsops.getallappointmentfees import getallappointmentfeesbp
 from controller.doctorsops.docappointmentfees import doctorappointmentfeesbp
 from controller.doctorsops.updateappoinmentfees import updatefeesbp
 from controller.search.search import searchbp
-from controller.appoinmentops.list_patients_by_slot import list_patients_bp
 from controller.appoinmentops.list_patients_by_slot import list_patients_by_datetime_bp
 
 load_dotenv()
@@ -224,7 +223,6 @@ app.register_blueprint(getallappoinmentbp)
 app.register_blueprint(updateappoinmentopsbp)
 app.register_blueprint(getappoinmenthistorybp)
 app.register_blueprint(deleteapponbp)
-app.register_blueprint(list_patients_bp)
 app.register_blueprint(list_patients_by_datetime_bp)
 #prescribe
 app.register_blueprint(prescribe_bp)
